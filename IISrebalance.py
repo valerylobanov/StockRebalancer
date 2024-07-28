9d6489ee3751d8f202cf6ba5b617e976300de574
Valeriy Lobnaov <valerylobanov@gmail.com>
2024-07-28 19:20:57 +0300

    ребалансировка с нормализацией процентов

--- IISrebalance.py
+++ IISrebalance.py
@@ -9,13 +9,14 @@ df = pd.read_csv('moex.csv',\
                     )
 
 dfc = pd.read_csv('current.csv',\
-                 dtype={'ticker': np.str_, 'quantity': np.int_, 'price': np.float64, 'lot_qty':np.int_},\
+                 dtype={'ticker': np.str_, 'qty': np.int_, 'price': np.float64, 'lot_qty':np.int_},\
                  index_col='ticker'
                     )
 
 p = pf.Portfolio(df)
-p.setCurrent(dfc,10000)
-# p.display()
-
-p.rebalance()
+p.setCurrent(dfc,600000)
 p.display()
+
+r2 = p.rebalance2()
+display(r2)
+print(r2['change_value'].sum())
--- Portfolio.py
+++ Portfolio.py
@@ -1,83 +1,80 @@
 import math
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
 import numpy as np
 from IPython.display import display
 
 
 class Portfolio:
     
-    # def __init__(self,assets,cash):
-
-        # self.assets = assets
-        # self.assets.set_index("ticker")
-        
-        # self.cash = cash
-        
-
-        # self.total = (self.assets['quantity']*self.assets['price']).sum() + self.cash  
-
-        # self.__recalcColumns()
-
-    # dfTarget = {ticker:string, index; percent:float (0-100)}
     def __init__(self,dfTarget):
         self.assets = dfTarget.rename(columns={"percent": "target_percent"})
-
+        self.assets['target_percent'] = self.assets['target_percent']/100
     # dfCurrent = {ticker: string,index; quantity: int, price: float}
     def setCurrent(self,dfCurrent,cash):
+
         self.assets = self.assets.join(dfCurrent)
         self.cash = cash
-        self.assets['percent'] = self.assets['price']*self.assets['quantity'] / self.__getTotal()
+        self.assets['percent'] = self.assets['price']*self.assets['qty'] / self.__getTotal()
            
     def __getTotal(self):
-        return (self.assets['quantity']*self.assets['price']).sum() + self.cash 
+        return (self.assets['qty']*self.assets['price']).sum() + self.cash 
 
     def __getError(self):
-        return np.square(np.subtract(self.assets['target_percent'],self.assets['percent'])).mean()*100
-        # return 0
-
-    # def setTarget(self,target):
-
-    #     target.set_index("t_ticker") # TODO: rename during join
-
-    #     self.assets = self.assets.join(target)
-        
-    #     # make sum of % = 100%
-    #     self.assets['target %'] = self.assets['target %']/ self.assets['target %'].sum()
-
-        
+        return np.square(np.subtract(self.assets['target_percent'],self.assets['percent'])).mean()*100   
 
     def display(self):     
-        display(self.assets)
-        print('cash = {0:.2f}'.format(self.cash))
-        print('sqrt error = {0:.2f}\n\n'.format(self.__getError())) 
-        print('total = {0:.2f}\n\n'.format(self.__getTotal()))
+
+        a = self.assets[self.assets.qty>0]
+        a['value'] = a['qty']*a['price']
+
+        n = np.nan
+        b = pd.DataFrame([[a['target_percent'].sum(),n,n,n,a['percent'].sum(),a['value'].sum(),n],\
+                          [0,n,n,n,self.cash/self.__getTotal(),self.cash,n],\
+                            [n,n,n,n,1,self.__getTotal(),n]],\
+                              columns=['target_percent','qty','price','lot_qty','percent','value','change_value'],\
+                                index=['[stock ttl]','[cash]','[total]'])
         
-    # def __recalcColumns(self):
+        df = pd.concat([a,b])
 
-    #     # self.assets['%'] = self.assets['price']*self.assets['quantity'] / self.total
-  
-    #     # assetsTotal = (self.assets['quantity']*self.assets['price']).sum()
-    #     # self.cash = self.total - assetsTotal
+        self.formatColumns(df)
+        display(df)
 
-    #     self.total = (self.assets['quantity']*self.assets['price']).sum() + self.cash  
+        print('sqrt error = {0:.2f}'.format(self.__getError())) 
 
-        # TODO: add "if exists"
-        #self.error = np.square(np.subtract(self.assets['target %'],self.assets['%'])).mean()*100
-  
-    def rebalance(self):
-        # print('hello')
+    def formatColumns(self, df):
+        df['percent'] = df['percent'].map('{:.1%}'.format)
+        df['target_percent'] = df['target_percent'].map('{:.1%}'.format)
+        df['qty'] = df['qty'].map('{:.0f}'.format)
+        df['lot_qty'] = df['lot_qty'].map('{:.0f}'.format)
+        df['price'] = df['price'].map('{:,.0f}'.format)
+        df['value'] = df['value'].map('{:,.0f}'.format)
 
-        # estimated quatity - 1st pass with rounding
-        self.assets['target_value'] = self.assets['target_percent'] * self.__getTotal()
+    def rebalance2(self):
+        totalPortfolio = self.__getTotal()
+        x = Portfolio.findNonZeroLots(self.assets,totalPortfolio)
+
+        # make sum(percent) = 100%
+        x['target_percent'] = x['target_percent'] / x['target_percent'].sum()
+
+        # target value
+        x['target_value'] = x['target_percent'] * totalPortfolio
+        x['target_lot_qty'] = (x['target_value'] / x['price']/x['lot_qty']).apply(np.floor) 
+        x['target_qty'] = x['target_lot_qty'] * x['lot_qty']
         
-        self.assets['temp_quantity'] = (self.assets['target_value'] / self.assets['price']).round()
-        self.assets['temp_value'] = self.assets['temp_quantity']*self.assets['price']
-        self.assets['temp_delta'] = self.assets['target_value'] - self.assets['temp_value']
-        temp_total = self.assets['temp_value'].sum()
+        # change = to be - as is
+        x['change_qty'] = x['target_qty'] - x['qty']
+        x['change_value'] = x['change_qty']*x['price']
 
-    #     # use cash remainder for additional assets. We're using asset with lowest price for this (simplest method)
-    #     minPrice = self.assets['price'].min()
-    #     addQty = math.floor(self.cash/minPrice)
-    #     self.assets.loc[ self.assets['price'] == minPrice, 'quantity' ] = self.assets['quantity'] + addQty
+        # only columns and rows needed for future processing
+        r = pd.DataFrame(x[['change_qty','change_value']].dropna())
+        r['change_value'] = r['change_value'].map('{:,.0f}'.format)
+        r['change_qty'] = r['change_qty'].map('{:,.0f}'.format)
+        
+        return r
 
-    #     self.__recalcColumns()
+    @staticmethod
+    def findNonZeroLots(x,totalPortfolio):
+        x['target_value'] = x['target_percent'] * totalPortfolio
+        x['target_lot_qty'] = (x['target_value'] / x['price']/x['lot_qty']).apply(np.floor) 
+        return x.loc[x['target_lot_qty']>0]
